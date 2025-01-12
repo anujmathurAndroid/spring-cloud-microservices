08b5396efde3436ffa6015b0415c4de0e3cee79f
Anuj Mathur <amathur@nectarcorp.com>
2025-01-12 19:52:09 +0530

    Update in eureka server

--- employee-service/src/main/java/com/learn/employeeService/EmployeeController.java
+++ employee-service/src/main/java/com/learn/employeeService/EmployeeController.java
@@ -1,3 +1,12 @@
+package com.learn.employeeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
